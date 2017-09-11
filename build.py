30f46e2b3797ec8e012623e99c31242ff319960d
Adeeb Shihadeh <quillford@users.noreply.github.com>
2017-09-10 19:06:09 -0700

    recusrive build

--- build.py
+++ build.py
@@ -2,15 +2,21 @@ import time
 import re
 import os
 
-def build():
-  base = open("template.html").read()
+base_ = "template.html"
+out_ = "index.html"
+
+def build(basefile, outfile):
+  base = open(basefile).read()
   
   links = re.findall('{(.*?)}', base)
 
   for link in links:
-    base = base.replace("{" + link + "}", getFile(link))
+    link_ = os.path.join(os.path.dirname(basefile), os.path.dirname(link)) + "/" + os.path.basename(link)
+    build(link_, link_ + "__TEMP")
+    base = base.replace("{" + link + "}", getFile(link_ + "__TEMP"))
+    os.remove(link_ + "__TEMP")
 
-  out = open("index.html", "w")
+  out = open(outfile, "w")
   out.write(base)
   out.close()
 
@@ -23,5 +29,5 @@ def getFile(filename):
   return file
 
 while True:
-  build()
+  build(base_, out_)
   time.sleep(0.1)
